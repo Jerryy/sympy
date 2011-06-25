f38639dd22f2d389e3e5f4a3cd7597a752d97833
Jeremias Yehdegho <j.yehdegho@gmail.com>
2011-06-25 15:32:43 +0200

    working f5
    An early, hopefully correct, implementation of F5.

--- sympy/polys/f5plus.py
+++ sympy/polys/f5plus.py
@@ -3,9 +3,11 @@ F5+ (although I'm starting with the classical F5)
 """
 
 # a rule is a tuple (t, k), where t is a monomial, k a natural number
-# a labeled polynomial is a tuple (p, (t, i)) corresponding to (p, t * F_i). ((t, i) is a signature)
+# a labeled polynomial is a tuple (p, s, n), where p is a sdp, s a signature and n a non-negative integer
 
 from sympy.polys.groebnertools import *
+
+
 
 def _term_rr_div(a, b, K):
     """Division of two terms in over a ring. """
@@ -34,14 +36,17 @@ def _term_ff_div(a, b, K):
 def sig(m, k):
     return (m, k)
 
-def lbp(p, s):
-    return (p, s)
+def lbp(p, s, n):
+    return (p, s, n)
 
-def poly(l):
-    return l[0]
+def poly(r):
+    return r[0]
 
 def S(r):
     return r[1]
+
+def num(r):
+    return r[2]
 
 def sig_cmp(s1, s2, O):
     """
@@ -54,32 +59,114 @@ def sig_cmp(s1, s2, O):
             return -1
     return 1
 
-def sig_monomial_mult(s, m):
+def sig_monomial_mul(s, m):
     return (monomial_mul(s[0], m), s[1])
 
-def add_rule(r, k):
-    Rules[r[1][1]].append((r[1][0], k))
+def add_rule(r, Rules):
+    #print("Rules: ", Rules)
+    #print(S(r)[1])
+    Rules[S(r)[1]].insert(0, (S(r)[0], num(r)))
 
-def rewritten(u, r, k):
-    L = Rules[r[1][1] - 1]
+    #print(Rules)
 
-    ut = monomial_mul(u[0], r[1][0])
+    # to whoever reads this: I spent HOURS wondering why a rule gets added for *all*
+    # indices. The reason was that I had Rules = [[]] * N[0]. Those inner [] are references
+    # to the same object (or something like that).
+
+def rewritten(u, r, Rules):
+    L = Rules[S(r)[1]]
+
+    ut = monomial_mul(u[0], S(r)[0])
 
     for l in L:
-        m = monomial_div(ut, l[0])
-        if m is not None:
-            return (m, G[l[1]]) 
-    return (u, r)
+        if monomial_div(ut, l[0]) is not None:
+            return l[1]
 
-def rewritable(u, r, k):
-    (v, rl) = rewritten(u, r, k)
-    return G.index(rl) == k 
+    return num(r)
 
-def critical_pair(k, r1, r2, NF, u, O, K):
-    #t = monomial_lcm(sdp_LM(poly(r1)), sdp_LM(poly(r2)))
+def is_rewritten(u, r, Rules):
+    l = rewritten(u, r, Rules)
+    return l != num(r)
+        
 
+def incremental_f5(F, u, O, K):
+    N = [len(F)]
+    Rules = [[] for n in xrange(N[0])]
+
+    r = lbp(F[-1], sig((0,) * (u + 1), N[0] - 1), N[0] - 1)
+    #r = lbp(F[0], sig((0,) * (u + 1), 0), 0)
+    G = [r]
+
+    for i in reversed(xrange(len(F) - 1)):
+    #for i in xrange(1, len(F)):
+        #print("current basis %d: " % i, is_groebner([poly(g) for g in G], u, O, K))
+        #print("length(basis) == ", len(G))
+        G = algorithm_f5(i, F[i], G, N, Rules, u, O, K)
+        #print("is_groebner: ", is_groebner([poly(g) for g in G], u, O, K))
+
+    #G = red_groebner([poly(g) for g in G], u, O, K)
+    G = [poly(g) for g in G]
+
+    if not is_groebner(G, u, O, K):
+        print("Not Groebner basis")
+
+    return sorted(G, key = lambda f: O(sdp_LM(f, u)), reverse = True)
+
+def algorithm_f5(i, f, G, N, Rules, u, O, K):
+    ri = lbp(f, sig((0,) * (u + 1), i), i) # number might be wrong
+    
+    n = len(G)
+    NF = lambda g: sdp_rem(g, [poly(r) for r in G[:n]], u, O, K) #lbp(sdp_rem(poly(g), [poly(r) for r in G[:n]], u, O, K), S(g))
+
+    G.append(ri)
+
+    P = [critical_pair(ri, r, i, NF, u, O, K) for r in G[:n]]
+    P = filter(None, P)
+
+    #print("P: ", [(num(cp[2]), num(cp[4])) for cp in P])
+
+    while P:
+        #print("P: ", [(num(cp[2]), num(cp[4])) for cp in P])
+        #print(P, N[0])
+        d = min([sum(cp[0]) for cp in P])
+        #print("d: ", d)
+
+
+        Pd = []
+        indices = []
+        for j, cp in enumerate(P):
+            if sum(cp[0]) == d:
+                Pd.append(cp)
+                indices.append(j)
+        
+        for j in reversed(indices):
+            del P[j]
+
+        F = spol(Pd, NF, N, Rules, u, O, K)
+        #print("F: ", [num(f) for f in F])
+        Rd = reduction(F, G, i, NF, N, Rules, u, O, K)
+        #print("Rd: ", [num(r) for r in Rd])
+
+        for r in Rd:
+            for p in G:
+                #print("i, algorithm_f5: ", i)
+                P.append(critical_pair(r, p, i, NF, u, O, K))
+
+            G.append(r)
+            #print("r_%d: " % num(r))
+            #print("len(P) == ", len(P))
+            
+
+        #P = set(filter(None, P))
+        P = filter(None, P)
+        #print(len(P))
+
+    #print(G)
+    return G
+
+def critical_pair(r1, r2, k, NF, u, O, K):
+    #print("critical_pair: ", num(r1), num(r2))
     p1, p2 = poly(r1), poly(r2)
-
     t = (monomial_lcm(sdp_LM(p1, u), sdp_LM(p2, u)), K.one)
 
     if K.has_Field:
@@ -89,150 +176,162 @@ def critical_pair(k, r1, r2, NF, u, O, K):
 
     u1, u2 = term_div(t, sdp_LT(p1, u, K), K), term_div(t, sdp_LT(p2, u, K), K)
 
-    if sig_cmp(sig_monomial_mult(S(r1), u1[0]), sig_monomial_mult(S(r2), u2[0]), O) < 0:
-        return critical_pair(k, r2, r1, NF, u, O, K)
+    if sig_cmp(sig_monomial_mul(S(r1), u1[0]), sig_monomial_mul(S(r2), u2[0]), O) < 0:
+        return critical_pair(r2, r1, k, NF, u, O, K)
 
     if S(r1)[1] > k:
+        #print("r%d, r%d not cp, S(r1)[1] > %d" % (num(r1), num(r2), k))
         return None
 
-    print([(monomial_mul(u1[0], S(r1)[0]), K.one)])
-    print(G)
-    if NF([(monomial_mul(u1[0], S(r1)[0]), K.one)]) != [(monomial_mul(u1[0], S(r1)[0]), K.one)]:
+    ut1 = [(monomial_mul(u1[0], S(r1)[0]), K.one)]
+    if NF(ut1) != ut1:
+        #print("r%d, r%d not cp, ut1 reducible" % (num(r1), num(r2)))
         return None
 
     if S(r2)[1] == k:
-        if NF([(monomial_mul(u2[0], S(r2)[0]), K.one)]) != [(monomial_mul(u2[0], S(r2)[0]), K.one)]:
+        ut2 = [(monomial_mul(u2[0], S(r2)[0]), K.one)]
+        if NF(ut2) != ut2:
+            #print("r%d, r%d not cp, S(r2)[1] == %d, and ut2 reducible" % (num(r1), num(r2), k))
             return None
 
-    return (t, u1, r1, u2, r2)
+    #print("new critical pair: [r%d, r%d]" % (num(r1), num(r2)))
+    return (t[0], u1, r1, u2, r2)
 
-def spol(P, NF, u, O, K):
+def spol(P, NF, N, Rules, u, O, K):
     F = []
 
-    for l, p in enumerate(P):
-        if not rewritable(p[1], p[2], l):
-            if not rewritable(p[3], p[4], l):
-                N += 1
-                f = sdp_sub(sdp_term_mul(poly(p[2]), p[1], u, O, K), sdp_term_mul(poly(p[4]), p[3], u, O, K))
-                rn = lbp(f, sig_monomial_mult(S(p[2]), p[1]))
-                add_rule(rn)
+    for l, cp in enumerate(P):
+        if not is_rewritten(cp[1], cp[2], Rules):
+            if not is_rewritten(cp[3], cp[4], Rules):
+                #print("both not rewritten")
+                N[0] += 1
+
+                f = sdp_sub(sdp_mul_term(poly(cp[2]), cp[1], u, O, K), sdp_mul_term(poly(cp[4]), cp[3], u, O, K), u, O, K)
+                rn = lbp(f, sig_monomial_mul(S(cp[2]), cp[1][0]), N[0])
+
+                add_rule(rn, Rules)
+
                 F.append(rn)
-    F.sort(lambda x, y: sig_cmp(S(x), S(y)))
+
+    F.sort(lambda f, g: sig_cmp(S(f), S(g), O))
+
+    #print("spol: ", len(P), len(F))
     return F
 
-N = 0
-Rules = []
-G = []
-
-def incremental_f5(F, u, O, K):
-    #global N
-    #global Rules
-    #global G
-
-    N = len(F)
-    Rules = [[]] * N
-    G = [lbp(F[-1], sig((0,) * (u + 1), N))]
-
-    for i in reversed(xrange(len(F))):
-        G = f5(i, F[i], G, u, O, K)
-
-    ret = [poly(g) for g in G]
-    N = 0
-    Rules = []
-    G = []
-    return ret
-
-def f5(i, f, G, u, O, K):
-    ri = lbp(f, sig((0,) * (u + 1), i))
-   
-    n = len(G)
-    NF = lambda f: sdp_rem(f, [poly(g) for g in G[:n]], u, O, K)
-
-    G.append(ri)
-
-    P = [critical_pair(i, ri, r, NF, u, O, K) for r in G]
-    P = filter(lambda p: p != None, P)
-    P.sort(key = lambda p: sum(p[0][0]))
-
-    while P:
-        d = sum(P[0][0][0])
-
-        Pd = []
-        indices = []
-        for i, p in enumerate(P):
-            if sum(p[0][0]) == d:
-                Pd.append(p)
-                indices.append(i)
-        for i in reversed(indices):
-            del P[i]
-        
-        F = spol(Pd, NF, u, O, K)
-        Rd = reduction(F, G, i, NF, u, O, K)
-
-        for r in Rd:
-            P.extend([critical_pair(r, p, i, NF, u, O, K) for p in G])
-            G.append(r)
-
-        P.sort(key = lambda p: sum(p[0][0]))
-
-    return G
-
-def reduction(todo, Gi, k, NF, u, O, K):
+def reduction(todo, G, k, NF, N, Rules, u, O, K):
     done = []
 
+    n = len(todo)
+
     while todo:
-        todo.sort(lambda f, g: sig_cmp(S(f), S(g)))
+        todo.sort(lambda f, g: sig_cmp(S(f), S(g), O))
         h = todo.pop()
 
-        h1, todo1 = top_reduction(sdp_rem(h, G, u, O, K), Gi + done, k, NF, u, O, K)
-
+        h = lbp(NF(poly(h)), S(h), num(h))
+        h1, todo1 = top_reduction(h, G + done, k, NF, N, Rules, u, O, K)
+        
         done.extend(h1)
         todo.extend(todo1)
-
+   
+    #print("reduction: ", n, len(done)) 
     return done
 
-def is_reducible(r, G, k, NF, u, O, K):
+def is_reducible(r, G, k, NF, Rules, u, O, K):
     if K.has_Field:
         term_div = _term_ff_div
     else:
         term_div = _term_rr_div
 
-    for ri in G:
-        u = term_div(sdp_LT(r, K), sdp_LT(ri, K))
-        if u is not None:
-            f = sdp_mul_term([u], (S(ri)[0], K.one), u, O, K)
-            if NF(f) == f:
-                if not rewritable(u, ri):
-                    if sig_monomial_mul(S(ri), f[0][0]) != S(r):
-                        return ri
-    return None
-    
+    #print("is_reducible called", len(G))
 
-def top_reduction(r, Gi, k, NF, u, O, K):
+    for j, rj in enumerate(G):
+        t = term_div(sdp_LT(poly(r), u, K), sdp_LT(poly(rj), u, K), K)
+        if t is not None:
+            f = sdp_mul_term([t], (S(rj)[0], K.one), u, O, K)
+            if NF(f) == f:
+                if not is_rewritten(t, rj, Rules):
+                    if sig_monomial_mul(S(rj), f[0][0]) != S(r):
+                        #print("is reducible")
+                        return rj
+
+    return None
+
+def top_reduction(r, G, k, NF, N, Rules, u, O, K):
+    if K.has_Field:
+        term_div = _term_ff_div
+    else:
+        term_div = _term_rr_div
+
     if poly(r) == []:
-        # not a regular sequence
         return [], []
 
-    rp = is_reducible(r, Gi, k, NF, u, O, K)
+    rp = is_reducible(r, G, k, NF, Rules, u, O, K)
 
-    if rp == None:
-        return lbp(sdp_monic(poly(r), K), r[1])
+    if rp is None:
+        return [lbp(sdp_monic(poly(r), K), S(r), num(r))], []
     else:
-        r1 = rp
+        rk1 = rp
 
-        um = monomial_div(sdp_LM(poly(r)), sdp_LM(poly(r1)))
+        ut = term_div(sdp_LT(poly(r), u, K), sdp_LT(poly(rk1), u, K), K)
 
-        if sig_cmp(sig_monomial_mult(S(r1), um), S(r)) < 0:
-            return [], [lbp(sdp_sub(poly(r), sdp_mul_term(poly(r1), (um, K.one), u, O, K), u, O, K), S(r))]
+        if sig_cmp(sig_monomial_mul(S(rk1), ut[0]), S(r), O) < 0:
+            r = lbp(sdp_sub(poly(r), sdp_mul_term(poly(rk1), ut, u, O, K), u, O, K), S(r), num(r))
+            return [], [r]
         else:
-            N += 1
-            rn = lbp(sdp_sub(sdp_mul_term(poly(r1), (um, K.one), u, O, K), poly(r), u, O, K), sig_monomial_mult(S(r1), u))
-            add_rule(rn, N)
-
+            N[0] += 1
+            rn = lbp(sdp_sub(sdp_mul_term(poly(rk1), ut, u, O, K), poly(r), u, O, K), sig_monomial_mul(S(rk1), ut[0]), N[0])
+            add_rule(rn, Rules)
             return [], [rn, r]
+
+def red_groebner(G, u, O, K):
+    """
+    Compute reduced Groebner basis, from BeckerWeispfenning93, p. 216
+    """
+
+    def reduction(P, u, O, K):
+        """
+        Reduction algorithm from BeckerWeispfenning93, p. 203
+        """
+    
+        Q = P
+        reducible = True
+
+        while reducible:
+            reducible = False
+            for i, p in enumerate(Q):
+                r = sdp_rem(p, Q[:i] + Q[i + 1:], u, O, K)
+                if r != []:
+                    del Q[i]
+                    Q.append(r)
+                    break
+
+        return [sdp_monic(p, K) for p in Q]
+
+    F = G
+    H = []
+
+    reducible = False
+
+    while F:
+        reducible = True
+        f0 = F.pop()
+
+        for f in F + H:
+            if monomial_div(sdp_LM(f0, u), sdp_LM(f, u)) is not None:
+                reducible = False
+                break
         
+        if reducible:
+            H.append(f0)
 
+    return reduction(H, u, O, K)
 
+def is_groebner(G, u, O, K):
+    for i in xrange(len(G)):
+        for j in xrange(i + 1, len(G)):
+            s = sdp_spoly(G[i], G[j], u, O, K)
+            s = sdp_rem(s, G, u, O, K)
+            if s != []:
+                return False
 
-
-
+    return True
