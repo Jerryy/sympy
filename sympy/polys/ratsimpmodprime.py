eb1d50e638d48e7490c33fde4c07fb8aa30e6641
Jeremias Yehdegho <j.yehdegho@gmail.com>
2011-07-22 16:13:06 +0200

    internal: new normalform and discarding all trivial solutions

--- sympy/polys/ratsimpmodprime.py
+++ sympy/polys/ratsimpmodprime.py
@@ -3,19 +3,29 @@ from sympy import symbols, solve, monomials, flatten
 from polytools import *
 from sympy import expand
 
-def normalform_expr(f, G, order):
-    r = 0
-    while f:
-        for g in G:
-            ltf = f.as_ordered_terms(order)[0]
-            ltg = g.as_ordered_terms(order)[0]
+def normalform(f, G, order, gens):
+    """
+    Computes the complete reduction of ``f`` modulo ``G``
+    w.r.t. ``order``.
+    """
+    def term((m, c)):
+        r = 1 
+        for i, v in enumerate(gens):
+            r *= v**m[i]
+        return c * r
 
-            if (ltf / ltg).is_polynomial():
-                f = expand(f - (ltf / ltg) * g)
+    r = Poly(0, f.gens)
+    while not f.is_zero:
+        for g in G:
+            if monomial_div(f.LM(order), g.LM(order)) is not None:
+                ltf = term(f.LT(order))
+                ltg = term(g.LT(order))
+                f = f - Poly(ltf/ltg, f.gens) * g
                 break
         else:
-            r = r + f.as_ordered_terms(order)[0]
-            f = f - f.as_ordered_terms(order)[0]
+            ltf = f.as_expr().as_ordered_terms(order)[0]
+            r = r + Poly(ltf, f.gens)
+            f = f - Poly(ltf, f.gens)
     return r
 
 def staircase(G, n, gens, order):
@@ -38,12 +48,13 @@ def staircase(G, n, gens, order):
 
 #def _ratsimp_mod_prime(a, b, G, gens, opt, N=0, D=0):
 def ratsimp_mod_prime(a, b, G, gens, order, N=0, D=0):
+    print("ratsimp called")
     c, d = a, b
     steps = 0
 
     #domain = a.domain
 
-    while N + D < Poly(a, gens).total_degree() + Poly(b, gens).total_degree():
+    while N + D < a.total_degree() + b.total_degree():
         M1 = staircase(G, N, gens, order)
         M2 = staircase(G, D, gens, order)
    
@@ -51,20 +62,34 @@ def ratsimp_mod_prime(a, b, G, gens, order, N=0, D=0):
         Cs = symbols("c:%d" % len(M1))
         Ds = symbols("d:%d" % len(M2))
 
-        c_hat = sum([Cs[i] * M1[i] for i in xrange(len(M1))])
-        d_hat = sum([Ds[i] * M2[i] for i in xrange(len(M2))])
+        c_hat = Poly(sum([Cs[i] * M1[i] for i in xrange(len(M1))]), gens)
+        d_hat = Poly(sum([Ds[i] * M2[i] for i in xrange(len(M2))]), gens)
 
-        r = normalform_expr(expand(a * d_hat - b * c_hat), G, order)
+        #print(a * d_hat - b * c_hat)
+        #print(type(a * d_hat - b * c_hat))
 
-        S = Poly(r, gens).coeffs()
-        print(S)
+        r = normalform(a * d_hat - b * c_hat, G, order, gens)
+
+        S = r.coeffs()
 
         sol = solve(S, Cs + Ds)
+
+        # remove solution of the form ``d2: -d3``
+        for key in sol.keys():
+            sol[key] = sol[key].subs(dict(zip(Cs + Ds, [0] * (len(Cs) + len(Ds)))))
 
         if sol is not None and not all([s == 0 for s in sol.itervalues()]):
             c = c_hat.subs(sol)
             d = d_hat.subs(sol)
+
+            # remove remaining variables (if the system was overdetermined)
+            c = Poly(c.subs(dict(zip(Cs + Ds, [0] * (len(Cs) + len(Ds))))), gens)
+            d = Poly(d.subs(dict(zip(Cs + Ds, [0] * (len(Cs) + len(Ds))))), gens)
+            print("c: ", c, Cs)
+            print("d: ", d, Ds)
             break
+
+        print("foo")
 
         N += 1
         D += 1
@@ -72,7 +97,7 @@ def ratsimp_mod_prime(a, b, G, gens, order, N=0, D=0):
 
     if steps > 0:
         print(c, d)
-        c, d = ratsimp_mod_prime(c, d, G, order, N, D - steps)
-        c, d = ratsimp_mod_prime(c, d, G, order, N - steps, D)
+        c, d = ratsimp_mod_prime(c, d, G, gens, order, N, D - steps)
+        c, d = ratsimp_mod_prime(c, d, G, gens, order, N - steps, D)
 
     return c, d
